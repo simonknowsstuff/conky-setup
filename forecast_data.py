7d4914cb28bb68f680a35a82f212a48713757cb6
simonknowsstuff <simonknowsstuff@gmail.com>
2023-02-17 20:52:00 +0530

    Initial setup
    New installer, changes to others

--- forecast_data.py
+++ forecast_data.py
@@ -6,6 +6,24 @@ import time
 from pathlib import Path
 
 
+REQUEST_PARAMS = {
+    "latitude": 53.83766,
+    "longitude": -9.35136,
+    "daily": {"temperature_2m_max", "temperature_2m_min"},
+    "timezone": "GMT",
+    "current_weather": "true"
+}
+REQUEST_URL = "https://api.open-meteo.com/v1/forecast"
+MAIN_DIRECTORY = Path(__file__).parent.resolve()
+WEATHER_ICON_DIRECTORY = f"{MAIN_DIRECTORY}/weather-icons/"
+OUTPUT_SVG = f"{MAIN_DIRECTORY}/weather_current.svg"
+LATTITUDE = 0.0
+LONGITUDE = 0.0
+with open(f'{MAIN_DIRECTORY}/lat_lon.txt', 'r') as file:
+    lines = [line.rstrip() for line in file]
+    LATITUDE = float(lines[0])
+    LONGITUDE = float(lines[1])
+
 def check_day():
     current_time = time.localtime()
     if current_time.tm_hour < 6 or current_time.tm_hour > 18:
@@ -19,66 +37,58 @@ def weather_code(code: int, is_day: bool):
     if not is_day:
         day_code = 'n'
     if code == 0:
-        shutil.copy2(f"weather-icons/01{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/01{day_code}.svg", OUTPUT_SVG)
         return "Clear Sky"
     elif code in [1, 2, 3]:
-        shutil.copy2(f"weather-icons/02{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/02{day_code}.svg", OUTPUT_SVG)
         return "Partly cloudy"
     elif code in [45, 48]:
-        shutil.copy2(f"weather-icons/50{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/50{day_code}.svg", OUTPUT_SVG)
         return "Fog"
     elif code in [51, 53, 55]:
-        shutil.copy2(f"weather-icons/10{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/10{day_code}.svg", OUTPUT_SVG)
         return "Drizzle"
     elif code in [56, 57]:
-        shutil.copy2(f"weather-icons/10{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/10{day_code}.svg", OUTPUT_SVG)
         return "Freezing drizzle"
     elif code in [61, 63, 65]:
-        shutil.copy2(f"weather-icons/10{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/10{day_code}.svg", OUTPUT_SVG)
         return "Rain"
     elif code in [66, 67]:
-        shutil.copy2(f"weather-icons/10{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/10{day_code}.svg", OUTPUT_SVG)
         return "Freezing rain"
     elif code in [71, 73, 75]:
-        shutil.copy2(f"weather-icons/13{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/13{day_code}.svg", OUTPUT_SVG)
         return "Slight snow fall"
     elif code == 77:
-        shutil.copy2(f"weather-icons/13{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/13{day_code}.svg", OUTPUT_SVG)
         return "Snow grains"
     elif code in [80, 81, 82]:
-        shutil.copy2(f"weather-icons/10{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/10{day_code}.svg", OUTPUT_SVG)
         return "Rain showers"
     elif code in [85, 86]:
-        shutil.copy2(f"weather-icons/13{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/13{day_code}.svg", OUTPUT_SVG)
         return "Snow showers"
     elif code == 95:
-        shutil.copy2(f"weather-icons/11{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/11{day_code}.svg", OUTPUT_SVG)
         return "Slight thunderstorm"
     elif code in [96, 99]:
-        shutil.copy2(f"weather-icons/11{day_code}.svg", "weather_current.svg")
+        shutil.copy2(f"{WEATHER_ICON_DIRECTORY}/11{day_code}.svg", OUTPUT_SVG)
         return "Thunderstorm"
 
 
-REQUEST_PARAMS = {
-    "latitude": 53.83766,
-    "longitude": -9.35136,
-    "daily": {"temperature_2m_max", "temperature_2m_min"},
-    "timezone": "GMT",
-    "current_weather": "true",
-    "timeformat": "unixtime"
-}
-REQUEST_URL = "https://api.open-meteo.com/v1/forecast"
-MAIN_DIRECTORY = Path(__file__).parent.resolve()
+def main():
+    response = requests.get(REQUEST_URL, params=REQUEST_PARAMS)
+    if response.status_code == 200:
+        resp_data = response.json()
+        data = {
+            "temp": resp_data['current_weather']['temperature'],
+            "max_temp": statistics.mean(resp_data['daily']['temperature_2m_max']),
+            "min_temp": statistics.mean(resp_data['daily']['temperature_2m_min']),
+            "weather": weather_code(resp_data['current_weather']['weathercode'], check_day())
+        }
+        with open(f'{MAIN_DIRECTORY}/forecast.json', 'w') as file:
+            file.writelines(json.dumps(data))
+            file.close()
 
-response = requests.get(REQUEST_URL, params=REQUEST_PARAMS)
-if response.status_code == 200:
-    resp_data = response.json()
-    data = {
-        "temp": resp_data['current_weather']['temperature'],
-        "max_temp": statistics.mean(resp_data['daily']['temperature_2m_max']),
-        "min_temp": statistics.mean(resp_data['daily']['temperature_2m_min']),
-        "weather": weather_code(resp_data['current_weather']['weathercode'], check_day())
-    }
-    with open(f'{MAIN_DIRECTORY}/forecast.json', 'w') as file:
-        file.writelines(json.dumps(data))
-        file.close()
+main()
--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
 CURRENT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")/")
-echo "Copying files to ~/.cache/conky-weather"
+echo "Installing requests"
+python -m pip install requests
+
+read -p "Enter the latitude of your location: " latitude
+read -p "Enter the longitude of your location: " longitude
+echo -e "$latitude\n$longitude" > lat_lon.txt
+
+echo "Copying files to ~/.local/share/conky-weather"
 mkdir -p ~/.local/share/conky-weather/
 cp -r $CURRENT_DIR/weather-icons ~/.local/share/conky-weather/
 cp -r $CURRENT_DIR/forecast_data.py ~/.local/share/conky-weather/
 cp -r $CURRENT_DIR/.conkyrc ~/.local/share/conky-weather/
+cp -r $CURRENT_DIR/lat_lon.txt ~/.local/share/conky-weather/
+python ~/.local/share/conky-weather/forecast_data.py
 echo "Run conky -c ~/.local/share/conky-weather/.conkyrc to run configuration."
